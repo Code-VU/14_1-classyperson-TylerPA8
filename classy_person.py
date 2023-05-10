b83cc48a542d31fc71173433dbf145e3a90b9197
Tyler Paneitz <Tyler.Paneitz@vu.com>
2023-05-10 07:40:23 -0500

    Not sure if I submitted this or not, but here ya go!

--- classy_person.py
+++ classy_person.py
@@ -8,8 +8,25 @@ don't forget the use of 'self' and to have the methods:
 '''
 
 class Person:
+    def __init__(self, age, name):
+        self.age = age
+        self.name = name
+        
 
+    def increase_age(self):
+        self.age += 1
 
+    def say_greeting(self):
+        print(f"Hello world! My name is {self.name}!")
+    
+    def count_to_age(self):
+        x = 1
+        while x <= self.age:
+            print (x)
+            x += 1
 
-
+"""Who = Person("Charles", 25)
+Person.increase_age(Who)
+Person.say_greeting(Who)
+Person.count_to_age(Who)"""
 # You won't need to call anything here.
